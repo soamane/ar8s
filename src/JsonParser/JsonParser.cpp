b3a093b7ecb621836629b51b5e9471a19f907a0d
soamane <soamane@mail.ru>
2024-09-23 01:46:03 +0300

    JsonParser wrapper implemented

---
+++ src/JsonParser/JsonParser.cpp
@@ -0,0 +1,50 @@
+#include "JsonParser.hpp"
+
+#include <iostream>
+
+JsonParser::JsonParser(const std::filesystem::path &path)
+{
+    this->OpenFile(path);
+}
+
+JsonParser::~JsonParser()
+{
+    this->CloseFile();
+}
+
+void JsonParser::OpenFile(const std::filesystem::path &path)
+{
+    if (!std::filesystem::exists(path))
+    {
+        throw std::runtime_error("File " + path.string() + " doesn't exists.");
+    }
+
+    this->m_file.open(path);
+    if (!this->m_file.is_open())
+    {
+        throw std::runtime_error("Failed to open file: " + path.string());
+    }
+}
+
+void JsonParser::CloseFile()
+{
+    if (this->m_file.is_open())
+    {
+        this->m_file.close();
+    }
+}
+
+const nlohmann::json JsonParser::Parse()
+{
+    nlohmann::json data;
+    try
+    {
+        this->m_file >> data;
+    }
+    catch (const nlohmann::json::parse_error &e)
+    {
+        throw std::runtime_error("JSON parse error: " + std::string(e.what()));
+    }
+
+    return data;
+}
---
+++ src/JsonParser/JsonParser.hpp
@@ -0,0 +1,25 @@
+#ifndef JSON_PARSER_HPP
+#define JSON_PARSER_HPP
+
+#include <fstream>
+#include <filesystem>
+#include <nlohmann/json.hpp>
+
+class JsonParser
+{
+protected:
+    JsonParser(const std::filesystem::path &path);
+    ~JsonParser();
+
+private:
+    void OpenFile(const std::filesystem::path &path);
+    void CloseFile();
+
+protected:
+    const nlohmann::json Parse();
+
+private:
+    std::fstream m_file;
+};
+
+#endif // !JSON_PARSER_HPP
