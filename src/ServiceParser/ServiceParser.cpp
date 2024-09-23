fd31a07c42faa22e4db3743f813e164651b320f9
soamane <soamane@mail.ru>
2024-09-23 16:05:02 +0300

    Services parser implemented

---
+++ src/ServiceParser/ServiceParser.cpp
@@ -0,0 +1,50 @@
+#include "ServiceParser.hpp"
+
+#include <iostream>
+
+ServiceParser::ServiceParser() : JsonParser("services.json")
+{
+    this->ParseServices();
+}
+
+ServiceParser::~ServiceParser()
+{
+}
+
+const std::vector<Service> &ServiceParser::GetParsedServices()
+{
+    if (this->services.empty())
+    {
+        throw std::runtime_error("Service's array is empty.");
+    }
+
+    return this->services;
+}
+
+void ServiceParser::ParseServices()
+{
+    nlohmann::json data = this->Parse();
+    if (data.empty())
+    {
+        throw std::runtime_error("Service's data is empty.");
+    }
+
+    if (!data.contains("services") || !data["services"].is_array())
+    {
+        throw std::invalid_argument("Key 'services' is missing or not an array.");
+    }
+
+    for (const auto &obj : data["services"])
+    {
+        Service service;
+
+        service.name = obj.value("name", "Google");
+        service.url = obj.value("url", "https://google.com/");
+        service.payload = obj.value("payload", "payload");
+        service.headers = obj.value("headers", std::vector<std::string>{});
+        service.protocolType = static_cast<ProtocolType>(obj.value("protocolType", ProtocolType::HTTPS));
+        service.requestType = static_cast<RequestType>(obj.value("requestType", RequestType::GET));
+
+        this->services.push_back(service);
+    }
+}
---
+++ src/ServiceParser/ServiceParser.hpp
@@ -0,0 +1,36 @@
+#ifndef SERVICE_PARSER_HPP
+#define SERVICE_PARSER_HPP
+
+#include <vector>
+#include <MyCurl.hpp>
+
+#include "../JsonParser/JsonParser.hpp"
+
+struct Service
+{
+    std::string name;
+    std::string url;
+
+    std::string payload;
+    std::vector<std::string> headers;
+
+    ProtocolType protocolType;
+    RequestType requestType;
+};
+
+class ServiceParser : JsonParser
+{
+public:
+    ServiceParser();
+    ~ServiceParser();
+
+    const std::vector<Service> &GetParsedServices();
+
+private:
+    void ParseServices();
+
+private:
+    std::vector<Service> services;
+};
+
+#endif // !SERVICE_PARSER_HPP
