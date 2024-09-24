3f236f80b405d0e6da85a3a998317a1f90f57923
soamane <soamane@mail.ru>
2024-09-24 21:08:20 +0300

    Set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,23 @@
 #include <memory>
 #include <iostream>
 
-#include "Parsers/Config/ConfigParser.hpp"
 #include "Parsers/Service/ServiceParser.hpp"
 #include "Parsers/Settings/SettingsParser.hpp"
+#include "Executor/Executor.hpp"
 
 int main() {
     try {
         SettingsParser settingsParser("settings.json");
         const Settings& settings = settingsParser.GetSettings();
 
-        std::cout << "settings.json >" << std::endl;
-        std::cout << settings.var << std::endl;
-
-        ServiceParser serviceParser("services.json");
+        ServiceParser serviceParser(settings, "services.json");
         const std::vector<Service>& services = serviceParser.GetServices();
 
-        std::cout << "services.json >" << std::endl;
-        for (const auto& service : services) {
-            std::cout << service.var << std::endl;
-        }
+        Executor executor(settings, services);
+        executor.Execute();
 
     } catch(const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
     }
     return 0;
 }
