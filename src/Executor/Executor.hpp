c5903e619ccf057b6ee1ae5d08fde298f19fcf9d
soamane <soamane@mail.ru>
2024-09-30 13:43:46 +0300

    Recode GetRandom function for more objects

--- src/Executor/Executor.hpp
+++ src/Executor/Executor.hpp
@@ -5,6 +5,7 @@
 #include "../Parsers/Settings/SettingsParser.hpp"
 
 #include <vector>
+#include <optional>
 #include <MyCurl.hpp>
 
 using RESPONSE = std::pair<std::string, long>;
@@ -19,12 +20,28 @@ public:
 private:
     RESPONSE ExecuteRequest(const MyCurl& myCurl, const Service& service) const;
     void ProcessServiceResponse(const RESPONSE& response) const;
-    Proxy GetRandomProxy(const std::vector<Proxy>& proxies) const;
+
+    template<class T>
+    std::optional<T> GetRandomObject(const std::vector<T>& vec);
+
     void ShowConfigs() const;
 
 private:
     Settings m_settings;
     std::vector<Service> m_services;
 };
+
+template<class T>
+std::optional<T> Executor::GetRandomObject(const std::vector<T>& vec) {
+    if (vec.empty()) {
+        return std::nullopt;
+    }
+
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<std::size_t> dis(0, vec.size() - 1);
+
+    return vec[ dis(generator) ];
+}
 
 #endif // EXECUTOR_HPP
