1099935952ae91f52f6f0d9779b84357b9991d76
soamane <soamane@mail.ru>
2024-10-05 14:58:42 +0300

    Add IsDigitOnly and NormalizePhone functions

--- src/telegram/chat/MessageHandler.cpp
+++ src/telegram/chat/MessageHandler.cpp
@@ -3,6 +3,28 @@
 #include <regex>
 
 bool MessageHandler::IsRussianPhoneNumber(std::string_view message) {
-    const std::regex pattern(R"(^(7|8)\d{10}|^9\d{9}$)");
+    const std::regex pattern(R"(^(\+7|7|8)?(\s|\-)?\(?(\d{3})\)?(\s|\-)?(\d{3})(\s|\-)?(\d{2})(\s|\-)?(\d{2})$|^(9\d{9})$)");
     return std::regex_match(message.data(), pattern);
+}
+
+bool MessageHandler::IsDigitOnly(std::string_view message) {
+    std::regex pattern(R"(^\d+$)");
+    return std::regex_match(message.data(), pattern);
+}
+
+const std::string MessageHandler::NormalizePhoneNumber(std::string_view phone) {
+    std::string normalized;
+
+    std::copy_if(phone.begin(), phone.end(), std::back_inserter(normalized), [](char c)
+    {
+        return std::isdigit(c);
+    });
+
+    if (normalized.size() == 11 && (normalized[ 0 ] == '7' || normalized[ 0 ] == '8')) {
+        return normalized.substr(1); // Удаляем первую цифру (7 или 8)
+    } else if (normalized.size() == 10 && normalized[ 0 ] == '9') {
+        return normalized;
+    } else {
+        return "";
+    }
 }
--- src/telegram/chat/MessageHandler.hpp
+++ src/telegram/chat/MessageHandler.hpp
@@ -1,11 +1,14 @@
 #ifndef MESSAGE_HANDLER_HPP
 #define MESSAGE_HANDLER_HPP
 
+#include <string>
 #include <string_view>
 
 struct MessageHandler {
 public:
     static bool IsRussianPhoneNumber(std::string_view message);
+    static bool IsDigitOnly(std::string_view message);
+    static const std::string NormalizePhoneNumber(std::string_view phone);
 };
 
 #endif // !MESSAGE_HANDLER_HPP
