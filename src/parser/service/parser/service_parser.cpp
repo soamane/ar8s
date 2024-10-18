fb5bb8d1b7433df6fdc738077e0de4b7f39decc1
soamane <soamane@mail.ru>
2024-10-19 02:39:08 +0300

    Rename and refactor

---
+++ src/parser/service/parser/service_parser.cpp
@@ -0,0 +1,55 @@
+#include "service_parser.hpp"
+
+ServiceParser::ServiceParser(const Settings& settings, const std::filesystem::path& path) : m_settings(settings), ConfigParser(path) {
+    this->Load();
+}
+
+const std::vector<Service>& ServiceParser::GetServices() {
+    if (this->m_services.empty()) {
+        throw std::runtime_error("Service's array is empty");
+    }
+
+    return this->m_services;
+}
+
+void ServiceParser::Load() {
+    nlohmann::json data = this->Parse();
+    if (data.empty()) {
+        throw std::runtime_error("Service's data is empty.");
+    }
+
+    if (!data.contains("services") || !data["services"].is_array() || data["services"].empty()) {
+        throw std::runtime_error("Failed to parse 'services', please check the correctness of the file.");
+    }
+
+    for (const auto& serviceObj : data["services"]) {
+        Service service;
+
+        service.name = serviceObj.at("name").get<std::string>();
+        service.url = serviceObj.at("url").get<std::string>();
+        service.payload = serviceObj.at("payload").get<std::string>();
+        service.headers = serviceObj.at("headers").get<std::vector<std::string>>();
+
+        this->ReplacePhoneNumber(service.url);
+        this->ReplacePhoneNumber(service.payload);
+
+        for (auto& header : service.headers) {
+            this->ReplacePhoneNumber(header);
+        }
+
+        service.requestType = serviceObj.at("request-type").get<RequestType>();
+        service.protocolType = serviceObj.at("protocol-type").get<ProtocolType>();
+
+        this->m_services.push_back(std::move(service));
+    }
+}
+
+void ServiceParser::ReplacePhoneNumber(std::string& source) {
+    std::size_t pos = 0;
+    std::string_view placeholder = "${phone}";
+
+    while ((pos = source.find("${phone}", pos)) != std::string::npos) {
+        source.replace(pos, placeholder.length(), this->m_settings.phoneNumber);
+        pos += this->m_settings.phoneNumber.length();
+    }
+}
---
+++ src/parser/service/parser/service_parser.hpp
@@ -0,0 +1,26 @@
+#ifndef SERVICE_PARSER_HPP
+#define SERVICE_PARSER_HPP
+
+#include "../service.hpp"
+#include "../../config/parser/config_parser.hpp"
+#include "../../settings/parser/settings_parser.hpp"
+
+#include <vector>
+#include <filesystem>
+
+class ServiceParser : public ConfigParser {
+public:
+    ServiceParser(const Settings& settings, const std::filesystem::path& path);
+
+    const std::vector<Service>& GetServices();
+
+private:
+    void Load() override;
+    void ReplacePhoneNumber(std::string& source);
+
+private:
+    Settings m_settings;
+    std::vector<Service> m_services;
+};
+
+#endif // !SERVICE_PARSER_HPP
