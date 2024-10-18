c6f27f9b08f2899be90481e8559675ee67332ff7
soamane <soamane@mail.ru>
2024-10-19 02:40:29 +0300

    Rewritten according to new changes

--- src/Executor/Executor.cpp
+++ src/Executor/Executor.cpp
@@ -1,5 +1,8 @@
 #include "Executor.hpp"
 
+#include "helper/vector/vector_helper.hpp"
+#include "handler/request/request_handler.hpp"
+
 #include <random>
 #include <thread>
 
@@ -11,66 +14,10 @@ Executor::~Executor() { }
 void Executor::Execute() {
     for (int i = 0; i < m_settings.attacksCount; ++i) {
         for (auto& service : m_services) {
-            std::shared_ptr<MyCurl> myCurl = std::make_unique<MyCurl>();
+            service.SetRandomUserAgent(this->m_settings);
 
-            this->SetProxyToRequest(myCurl);
-            this->SetUserAgentToRequest(service);
-            this->ExecuteRequest(myCurl, service);
-
+            RequestHandler requestHandler;
+            requestHandler.ExecuteRequest(this->m_settings, service);
         }
-    }
-}
-
-RESPONSE Executor::ExecuteRequest(std::shared_ptr<MyCurl> myCurl, const Service& service) const {
-    const curl_slist* headers = myCurl->AddHeaders(service.headers);
-    if (!headers) {
-        throw std::runtime_error("Failed to add headers for request: " + service.name);
-    }
-
-    switch (service.requestType) {
-        case RequestType::GET:
-            return myCurl->PerformGetRequest(service.protocolType, service.url, headers);
-
-        case RequestType::POST:
-            return myCurl->PerformPostRequest(service.protocolType, service.url, headers, service.payload);
-
-        default:
-            throw std::invalid_argument("Invalid request type for service '" + service.name + "'");
-    }
-}
-
-void Executor::SetProxyToRequest(std::shared_ptr<MyCurl> myCurl) {
-    if (!this->m_settings.useProxy) {
-        return;
-    }
-
-    std::optional<Proxy> optionalProxy = this->GetRandomObject<Proxy>(this->m_settings.proxies);
-    if (!optionalProxy.has_value()) {
-        return;
-    }
-
-    const Proxy& proxy = optionalProxy.value();
-    myCurl->UseProxyServer(proxy.address, proxy.username, proxy.password);
-}
-
-void Executor::SetUserAgentToRequest(Service& service) {
-    if (!this->m_settings.useUserAgent) {
-        return;
-    }
-
-    for (auto& header : service.headers) {
-        if (header.find("User-Agent:") != 0) {
-            continue;
-        }
-
-        auto optionalUserAgent = this->GetRandomObject<UserAgent>(this->m_settings.userAgents);
-        if (!optionalUserAgent.has_value()) {
-            return;
-        }
-
-        const UserAgent& userAgent = optionalUserAgent.value();
-        header = "User-Agent: " + userAgent.name;
-
-        break; // becouse 'user-agent' field replaced
     }
 }
