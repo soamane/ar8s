ae611abe85db1c6ffaa6d39b08b4b92a17c83794
soamane <soamane@mail.ru>
2024-10-19 02:41:17 +0300

    Replace into parser/ folder

---
+++ src/parser/settings/parser/settings_parser.cpp
@@ -0,0 +1,70 @@
+#include "settings_parser.hpp"
+
+SettingsParser::SettingsParser(const std::filesystem::path& path) : ConfigParser(path) {
+    this->Load();
+}
+
+Settings& SettingsParser::GetSettings() {
+    return this->m_settings;
+}
+
+void SettingsParser::Load() {
+    nlohmann::json data = this->Parse();
+    if (data.empty()) {
+        throw std::runtime_error("Settings data is empty.");
+    }
+
+    this->ParseProxies(data);
+    this->ParseUserAgents(data);
+    this->ParseAdditionals(data);
+}
+
+void SettingsParser::ParseAdditionals(const nlohmann::json& data) {
+    this->CheckJsonKey(data, "attacks-count");
+    this->m_settings.attacksCount = data.at("attacks-count");
+}
+
+void SettingsParser::ParseProxies(const nlohmann::json& data) {
+    this->CheckJsonKey(data, "use-proxy");
+    this->m_settings.useProxy = data.at("use-proxy");
+
+    this->CheckJsonArray(data, "proxies");
+    for (const auto& proxyObj : data["proxies"]) {
+        Proxy proxy;
+        {
+            proxy.address = proxyObj.at("address").get<std::string>();
+            proxy.username = proxyObj.value("username", "");
+            proxy.password = proxyObj.value("password", "");
+        }
+
+        this->m_settings.proxies.push_back(std::move(proxy));
+    }
+}
+
+void SettingsParser::ParseUserAgents(const nlohmann::json& data) {
+    this->CheckJsonKey(data, "use-useragent");
+    this->m_settings.useUserAgent = data.at("use-useragent").get<bool>();
+
+    this->CheckJsonArray(data, "user-agents");
+    for (const auto& userAgentString : data["user-agents"]) {
+        UserAgent userAgent;
+        {
+            userAgent.name = userAgentString.get<std::string>();
+        }
+
+        this->m_settings.userAgents.push_back(userAgent);
+    }
+}
+
+
+void SettingsParser::CheckJsonKey(const nlohmann::json& data, const std::string& key) const {
+    if (!data.contains(key)) {
+        throw std::runtime_error("Failed to parse '" + key + "', please check the correctness of the file.");
+    }
+}
+
+void SettingsParser::CheckJsonArray(const nlohmann::json& data, const std::string& key) const {
+    if (!data.contains(key) || !data[key].is_array()) {
+        throw std::runtime_error("Failed to parse '" + key + "', please check the correctness of the file.");
+    }
+}
---
+++ src/parser/settings/parser/settings_parser.hpp
@@ -0,0 +1,33 @@
+#ifndef SETTINGS_PARSER_HPP
+#define SETTINGS_PARSER_HPP
+
+#include "../settings.hpp"
+#include "../../config/parser/config_parser.hpp"
+
+#include <vector>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+class SettingsParser : public ConfigParser {
+public:
+    explicit SettingsParser(const std::filesystem::path& path);
+    Settings& GetSettings();
+
+private:
+    void Load() override;
+
+private:
+    void ParseAdditionals(const nlohmann::json& data);
+    void ParseProxies(const nlohmann::json& data);
+    void ParseUserAgents(const nlohmann::json& data);
+
+private:
+    void CheckJsonKey(const nlohmann::json& data, const std::string& key) const;
+    void CheckJsonArray(const nlohmann::json& data, const std::string& key) const;
+
+private:
+    Settings m_settings;
+};
+
+#endif // !SETTINGS_PARSER_HPP
