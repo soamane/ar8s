9fd47459f83a7bf776cf70219ce9dd0077dced26
soamane <soamane@mail.ru>
2024-10-28 12:52:12 +0300

    Add InputFormat

---
+++ src/frontend/telegram/chat/input/format/input_format.hpp
@@ -0,0 +1,11 @@
+#ifndef INPUT_FORMAT_HPP
+#define INPUT_FORMAT_HPP
+
+#include <string>
+#include <string_view>
+
+namespace InputFormat {
+    const std::string FormatPhoneNumber(std::string_view phone);
+}
+
+#endif // !INPUT_FORMAT_HPP
