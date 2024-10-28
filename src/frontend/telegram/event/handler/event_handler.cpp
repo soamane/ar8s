2afbbfd2770a9487329ab920e03a1c4cac504690
soamane <soamane@mail.ru>
2024-10-28 20:07:03 +0300

    Update handle return value

--- src/frontend/telegram/event/handler/event_handler.cpp
+++ src/frontend/telegram/event/handler/event_handler.cpp
@@ -2,11 +2,11 @@
 
 EventHandler::EventHandler(TgBot::Bot& bot, std::shared_ptr<UserData> userData)
     : m_bot(bot), m_user(userData), m_messageHandler(std::make_shared<MessageHandler>(bot, userData)),
-    m_inputProcessor(std::make_unique<InputProcessor>(userData, m_messageHandler)) { }
+    m_inputProcessor(std::make_unique<InputProcessor>(userData)) { }
 
 void EventHandler::Handle() {
-    this->m_messageHandler->SendChatMessage("⚠️ Перед использованием вы даете согласие, что не будете использовать функционал в незаконных целях.");
-    this->m_messageHandler->SendChatMessage("📲 Введи номер телефона в любом удобном для тебя формате.\n\n*принимаются исключительно российские номера телефонов");
+    this->m_messageHandler->SendChatMessage("⚠️ Перед использованием Вы даете согласие, что не будете использовать функционал в незаконных целях.");
+    this->m_messageHandler->SendChatMessage("📲 Введите номер телефона в любом удобном формате.\n\n*принимаются исключительно российские номера телефонов");
 
     this->InitEvents();
 }
@@ -16,18 +16,29 @@ void EventHandler::InitEvents() {
     {
         const auto& inputStatus = this->m_user->input->status;
         if (!inputStatus->attackTimeEntered || !inputStatus->phoneEntered) {
-
-        } else {
-
+            this->m_messageHandler->SendChatMessage("❌ Эту команду нельзя выполнить сейчас.\n\nПеред использованием этой команды вы должны ввести необходимые данные.");
+            return;
         }
     });
 
     this->onNonCommandMessageEvent([this](TgBot::Message::Ptr message)
     {
         if (!this->m_user->input->status->phoneEntered) {
-            this->m_inputProcessor->ProcessPhoneNumber(message->text);
+            if (!this->m_inputProcessor->ProcessPhoneNumber(message->text)) {
+                this->m_messageHandler->SendChatMessage("❌ Некорректный номер телефона.\n\nУбедитесь в правильности введеного номера и повторите попытку. В случае непредвиденного поведения обратитесь к разработчику: @soamane");
+                return;
+            }
+
+            this->m_messageHandler->SendChatMessage("📲 Номер телефона успешно считан.");
+            this->m_messageHandler->SendChatMessage("⏳ Введите количество минут для атаки. Учтите, что максимальное количество не должно превышать 100");
+
         } else if (!this->m_user->input->status->attackTimeEntered) {
-            this->m_inputProcessor->ProcessAttackTime(message->text);
+            if (!this->m_inputProcessor->ProcessAttackTime(message->text)) {
+                this->m_messageHandler->SendChatMessage("❌ Некорректный ввод.\n\nУбедитесь в правильности введеных данных. В случае непредвиденного поведения обратитесь к разработчику: @soamane");
+                return;
+            }
+
+            this->m_messageHandler->SendChatMessage("🎲 Количество минут для проведения атаки: " + std::to_string(m_user->input->attackTime) + "\n\nЧтобы начать спам по указанному номеру, введите команду /execute");
         }
     });
 }
