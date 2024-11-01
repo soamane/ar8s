52387a0154cf0e5de0b98dc07f21665462dd1d6d
soamane <soamane@mail.ru>
2024-11-01 15:17:54 +0300

    Refactor

--- src/frontend/telegram/connection/listener/connection_listener.cpp
+++ src/frontend/telegram/connection/listener/connection_listener.cpp
@@ -1,33 +1,35 @@
 #include "connection_listener.hpp"
 #include "../../user/data/user_data.hpp"
+#include <iostream>
 
 ConnectionListener::ConnectionListener(TgBot::Bot& bot)
     : m_bot(bot) { }
 
 void ConnectionListener::Listen() {
     m_bot.getEvents().onCommand("start", [this](TgBot::Message::Ptr message)
- {
-     AcceptConnection(message->chat->id);
+    {
+        AcceptConnection(message->chat->id);
     });
 }
 
 void ConnectionListener::AcceptConnection(int64_t currentChatId) {
     auto currentUserData = m_connectionManager.GetUserDataById(currentChatId);
-    if (currentUserData) {
-        CreateEventHandler(currentUserData);
-    } else {
-        auto userData = std::make_shared<UserData>(currentChatId);
-        m_connectionManager.RegisterConnection(userData);
-        CreateEventHandler(userData);
+
+    if (!currentUserData) {
+        currentUserData = std::make_shared<UserData>(currentChatId);
+        m_connectionManager.RegisterConnection(currentUserData);
     }
+
+    CreateEventHandler(currentUserData);
 }
 
 void ConnectionListener::CreateEventHandler(std::shared_ptr<UserData> userData) {
-    if (auto currentEventHandler = m_connectionManager.GetEventHandlerByUserData(userData)) {
-        currentEventHandler->Handle();
-    } else {
-        auto eventHandler = std::make_shared<EventHandler>(m_bot, userData);
-        eventHandler->Handle();
+    auto eventHandler = m_connectionManager.GetEventHandlerByUserData(userData);
+
+    if (!eventHandler) {
+        eventHandler = std::make_shared<EventHandler>(m_bot, userData);
         m_connectionManager.RegisterEventHandler(eventHandler, userData);
     }
+
+    eventHandler->Handle();
 }
